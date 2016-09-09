ae0ae0c75d4b42054f7bcc3d2058065b0583f71b
Justin Azoff <justin.azoff@gmail.com>
2016-09-08 20:44:52 -0400

    initial package

---
+++ README.md
@@ -0,0 +1,17 @@
+linter-bro
+=========================
+
+This linter plugin for [Linter](https://github.com/AtomLinter/Linter) provides an interface to [bro](https://bro). It will be used with files that have the ".bro" syntax.
+
+## Installation
+Linter package must be installed in order to use this plugin. If Linter is not installed, please follow the instructions [here](https://github.com/AtomLinter/Linter).
+
+### bro installation
+Before using this plugin, you must ensure that `bro` is installed on your system.
+
+Now you can proceed to install the linter-bro plugin.
+
+### Plugin installation
+```
+$ apm install linter-bro
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+'use babel'
+
+var BroErrorRegex = /(error|warning) in (.+), line ([0-9]+): (.+)/g;
+
+export var config = {
+  broExecutablePath: {
+    description: 'Path to the bro executable',
+    type: 'string',
+    default: 'bro'
+  }
+}
+
+
+export function activate() {
+  console.log('activate linter-bro using path: ', atom.config.get('linter-bro.broExecutablePath'));
+}
+
+
+export function provideLinter() {
+  var path = require('path')
+  var helpers = require('atom-linter');
+  return {
+    name: 'Bro',
+    scope: 'project',
+    lintOnFly: true,
+    grammarScopes: ['source.bro'],
+    lint(textEditor) {
+      //Why bother with observe?
+      var bro = atom.config.get('linter-bro.broExecutablePath');
+      var filePath = textEditor.getPath();
+      var cwd = path.dirname(filePath);
+      var args = ["-a", filePath];
+      var opts = {
+        cwd: cwd,
+        stream: 'stderr',
+        allowEmptyStderr: true,
+        ignoreExitCode: true
+      };
+      return helpers.exec(bro, args, opts).then(function(output) {
+        var messages = [];
+        while ((match = BroErrorRegex.exec(output)) !== null) {
+          var line = parseInt(match[3], 10);
+          messages.push({
+            type: match[1],
+            filePath: match[2],
+            range: [[line-1, 0], [line-1, 0]],
+            text: match[4]
+          });
+        };
+        console.log(messages);
+        return messages;
+      });
+    }
+  }
+}
